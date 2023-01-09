6add73df861d64215f7d2c5c0496e09564440b25
Tnatiel <117045287+Tnatiel@users.noreply.github.com>
2023-01-10 00:34:16 +0200

    Added post endpoint and initial word validtion

--- routers/word-router.ts
+++ routers/word-router.ts
@@ -1,8 +1,24 @@
-import { Router, Request, Response } from "express";
+import express, { Router, Request, Response } from "express";
 
 export const wordRouter = Router();
 
 
+const word = 'MORAL'
+
 wordRouter.get('/', (req: Request, res: Response) => {
-    res.status(200).send('MORAL')
+    res.status(200).send(word)
+})
+
+wordRouter.post('/check', express.json() , (req: Request, res: Response) => {
+    const currentWord = req.body();
+    
+    if (currentWord === word) {
+        res.status(200).send(true)
+    }
+    if (currentWord !== word) {
+        res.status(200).send(false)
+    }
+
+    res.status(400). send((e: Error) => console.log(e.message))
+
 })
